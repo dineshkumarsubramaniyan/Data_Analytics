b14fa008679d3f11e53a1390f9ac7e1735939aa0
unknown <dineshkumarsubramaniyan@gmail.com>
2025-04-27 19:39:47 +0530

    added first_script.py

---
+++ first_script.py
@@ -0,0 +1,24 @@
+# name = "Dinesh"
+
+
+# my_rank = 10.5
+# print(my_rank)
+# print(type(my_rank))
+
+
+# print(name)
+# print(type(name))
+
+# value = True
+# print(value)
+# print(type(value))
+
+
+my_rank = [10, 15, 24]
+
+print(my_rank)
+print(type(my_rank))
+
+my_rank[]
+
+2+5
